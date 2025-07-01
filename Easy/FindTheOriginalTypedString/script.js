1ca22fc2c81a48490eb82344f063804d9a1d3239
HimeshDua <himeshdua22@gmail.com>
2025-07-01 14:25:48 +0500

    question 1 : got count of repetetive characters in string using reduce method

--- Easy/FindTheOriginalTypedString/script.js
+++ Easy/FindTheOriginalTypedString/script.js
@@ -3,20 +3,48 @@
  * @return {number}
  */
 function possibleStringCount(word) {
-  let arr = [];
-  let diffArr = [];
-  for (let w of word) {
-    arr.push(w);
-    if (arr.includes(w)) {
-      diffArr.push(arr);
-      arr = arr.filter((arr) => arr === w);
-    }
-  }
-  console.log('length', arr.length, '\n', 'arr', arr);
-  console.log('\n');
-  console.log('diffArr length', diffArr.length, 'diffArr', diffArr);
+  const posStr = [...word].reduce((storedVal, currentValue) => {
+    storedVal[currentValue] = (storedVal[currentValue] || 0) + 1;
+    return storedVal;
+  }, {});
 
-  // console.log(arr.length);
+  console.log(posStr);
 }
+// reduce method summary
+// 'a','b','b','c' reduce loops on without mutating actual array
+// first iteration:
+// storedVal['a'] = (undefined || 0) + 1 = 1
+// second iteration:
+// storedVal['b'] = (undefined || 0) + 1 = 1
+// third iteration:
+// storedVal['b'] = (1 || 0) + 1 = 2
+
+// function possibleStringCount(word) {
+//   let arr = Array.from(word);
+//   let toSet = new Set(arr);
+//   console.log(arr.length, 'word', arr);
+//   console.log(toSet.length, 'sets', toSet);
+//   console.log('\n');
+//   // console.log(posStr.length, 'possibleWords', posStr);
+// }
+
+// possibleStringCount('abbcccc');
+
+// function possibleStringCount(word) {
+//   let arr = []; // splited Word
+//   let posStr = []; // possible Word
+//   for (let w of word) {
+//     arr.push(w);
+//     if (arr.includes(w)) {
+//       posStr.push(arr);
+//       arr = arr.filter((arr) => arr === w);
+//     }
+//   }
+//   console.log(arr.length, 'word', arr);
+//   console.log('\n');
+//   console.log(posStr.length, 'possibleWords', posStr);
+
+//   // console.log(arr.length);
+// }
 
 possibleStringCount('abbcccc');
